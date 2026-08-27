20b866e8ea6a70856c07170f9a98982114df5b28
ritikarawlani <ritikarawlani@gmail.com>
2026-08-27 17:45:57 +0530

    release smart-base

--- sitepreview/base/StructureDefinition/SGcode/index.php
+++ sitepreview/base/StructureDefinition/SGcode/index.php
@@ -7,19 +7,19 @@ function Redirect($url)
 
 $accept = $_SERVER['HTTP_ACCEPT'];
 if (strpos($accept, 'application/json+fhir') !== false)
-  Redirect('https://smart.who.int/base/1.0.0/StructureDefinition-SGcode.json2');
+  Redirect('http://smart.who.int/base/0.2.0/StructureDefinition-SGcode.json2');
 elseif (strpos($accept, 'application/fhir+json') !== false)
-  Redirect('https://smart.who.int/base/1.0.0/StructureDefinition-SGcode.json1');
+  Redirect('http://smart.who.int/base/0.2.0/StructureDefinition-SGcode.json1');
 elseif (strpos($accept, 'json') !== false)
-  Redirect('https://smart.who.int/base/1.0.0/StructureDefinition-SGcode.json');
+  Redirect('http://smart.who.int/base/0.2.0/StructureDefinition-SGcode.json');
 elseif (strpos($accept, 'application/xml+fhir') !== false)
-  Redirect('https://smart.who.int/base/1.0.0/StructureDefinition-SGcode.xml2');
+  Redirect('http://smart.who.int/base/0.2.0/StructureDefinition-SGcode.xml2');
 elseif (strpos($accept, 'application/fhir+xml') !== false)
-  Redirect('https://smart.who.int/base/1.0.0/StructureDefinition-SGcode.xml1');
+  Redirect('http://smart.who.int/base/0.2.0/StructureDefinition-SGcode.xml1');
 elseif (strpos($accept, 'html') !== false)
-  Redirect('https://smart.who.int/base/1.0.0/StructureDefinition-SGcode.html');
+  Redirect('http://smart.who.int/base/0.2.0/StructureDefinition-SGcode.html');
 else 
-  Redirect('https://smart.who.int/base/1.0.0/StructureDefinition-SGcode.xml');
+  Redirect('http://smart.who.int/base/0.2.0/StructureDefinition-SGcode.xml');
 ?>
     
 You should not be seeing this page. If you do, PHP has failed badly.
